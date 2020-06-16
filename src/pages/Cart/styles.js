3294fc4e26f2b0e6deb6a3850d8cbaef7cbac29d
Adeilton Sousa <adeiltonsousaxavier@gmail.com>
2020-06-16 02:17:47 -0300

    Ajustes responsividade carrinho

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -32,17 +32,10 @@ export const Container = styled.div`
 export const ProductTable = styled.table`
 	width: 100vw;
 
-	@media (max-width: 450px) {
-		display: none;
-	}
-
 	thead th {
 		color: #999;
 		text-align: left;
 		padding: 12px;
-		@media (max-width: 450px) {
-			display: none;
-		}
 	}
 
 	tbody td {
